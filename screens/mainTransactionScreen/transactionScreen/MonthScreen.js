4f410c5b1e2fe3d2962f568b6e6f34cffcd5907a
kcnabin <kcnabinkc@gmail.com>
2023-10-31 20:38:13 +0545

    yearly total income and expenses can be viewed on months tab

--- screens/mainTransactionScreen/transactionScreen/MonthScreen.js
+++ screens/mainTransactionScreen/transactionScreen/MonthScreen.js
@@ -1,16 +1,19 @@
-import { View, Text, StyleSheet } from "react-native";
-import { mainStyle } from "../../../mainStyle";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
+
 import { getMonthlyTotalFromDb } from "../../../util/database";
-import { ScrollView } from "react-native-gesture-handler";
+
 import { allColors } from "../../../Colors";
+import AppLoading from "expo-app-loading";
+import { displaySelectedMonthAndYear } from "../../../helper/dateHelper";
 
 const MonthTab = () => {
   const selectedMonth = useSelector((state) => state.selectedMonth);
   const [monthlyRecord, setMonthlyRecord] = useState([]);
 
   useEffect(() => {
+    setMonthlyRecord([]);
     let year = selectedMonth.year;
     let month = selectedMonth.month;
 
@@ -39,25 +42,62 @@ const MonthTab = () => {
     }
   }, [selectedMonth]);
 
-  if (monthlyRecord.length < 12) {
-    return (
-      <View style={style.container}>
-        <View style={style.center}>
-          <Text style={style.font24}>Fetching data...</Text>
-        </View>
-      </View>
+  let totalIncome = 0;
+  let totalExpenses = 0;
+
+  if (monthlyRecord.length === 12) {
+    totalIncome = monthlyRecord.reduce((sum, record) => sum + record.income, 0);
+
+    totalExpenses = monthlyRecord.reduce(
+      (sum, record) => sum + record.expenses,
+      0
     );
+  }
+
+  if (monthlyRecord.length < 12) {
+    return <AppLoading />;
+    // return (
+    //   <View style={style.container}>
+    //     <View style={style.center}>
+    //       <Text style={style.font24}>Fetching data...</Text>
+    //     </View>
+    //   </View>
+    // );
   }
 
   return (
     <ScrollView style={style.container}>
+      <View style={style.summaryContainer}>
+        <View style={style.summary}>
+          <Text style={style.summaryTitle}>Income</Text>
+          <Text style={[style.summaryText, style.incomeTotal]}>
+            {totalIncome.toLocaleString()}
+          </Text>
+        </View>
+        <View style={style.summary}>
+          <Text style={style.summaryTitle}>Expenses</Text>
+          <Text style={[style.summaryText, style.expensesTotal]}>
+            {totalExpenses.toLocaleString()}
+          </Text>
+        </View>
+        <View style={style.summary}>
+          <Text style={style.summaryTitle}>Total</Text>
+          <Text style={[style.summaryText]}>
+            {(totalIncome - totalExpenses).toLocaleString()}
+          </Text>
+        </View>
+      </View>
+
       <View style={style.monthlyRecordsContainer}>
         {monthlyRecord.map((eachRecord, i) => (
           <View key={i} style={style.eachMonth}>
             <View style={style.eachValue}>
-              <Text
-                style={[style.text, style.dateText]}
-              >{`${eachRecord.year}-${eachRecord.month}`}</Text>
+              <Text style={[style.text, style.dateText]}>
+                {displaySelectedMonthAndYear({
+                  month: eachRecord.month,
+                  year: eachRecord.year,
+                })}
+              </Text>
             </View>
             <View style={style.eachValue}>
               <Text style={[style.text, style.incomeText]}>
@@ -83,7 +123,7 @@ const style = StyleSheet.create({
     flex: 1,
   },
   monthlyRecordsContainer: {
-    marginVertical: 12,
+    marginBottom: 12,
   },
 
   center: {
@@ -117,5 +157,30 @@ const style = StyleSheet.create({
   expensesText: {
     color: allColors.expensesColor,
     textAlign: "right",
+  },
+  summaryContainer: {
+    flexDirection: "row",
+  },
+  summary: {
+    flex: 1,
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: allColors.lightGray,
+  },
+  summaryTitle: {
+    textAlign: "center",
+    marginBottom: 4,
+    fontSize: 16,
+  },
+  summaryText: {
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  incomeTotal: {
+    color: allColors.incomeColor,
+  },
+  expensesTotal: {
+    color: allColors.expensesColor,
   },
 });
